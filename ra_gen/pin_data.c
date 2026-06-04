aec52ae2ad3d22e1849a220a87f21b2d681a3af1
Wendell Lee <2951404774@stu.xupt.edu.cn>
2026-06-04 22:42:18 +0800

    修改camera_reset的引脚配置为P012_A

--- ra_gen/pin_data.c
+++ ra_gen/pin_data.c
@@ -5,8 +5,11 @@
 const ioport_pin_cfg_t g_bsp_pin_cfg_data[] =
         {
 
-        { .pin = BSP_IO_PORT_01_PIN_00, .pin_cfg = ((uint32_t) IOPORT_CFG_PERIPHERAL_PIN
-                | (uint32_t) IOPORT_PERIPHERAL_OSPI) },
+        { .pin = BSP_IO_PORT_00_PIN_12, .pin_cfg = ((uint32_t) IOPORT_CFG_PORT_DIRECTION_OUTPUT
+                | (uint32_t) IOPORT_CFG_PORT_OUTPUT_HIGH) },
+
+          { .pin = BSP_IO_PORT_01_PIN_00, .pin_cfg = ((uint32_t) IOPORT_CFG_PERIPHERAL_PIN
+                  | (uint32_t) IOPORT_PERIPHERAL_OSPI) },
 
           { .pin = BSP_IO_PORT_01_PIN_01, .pin_cfg = ((uint32_t) IOPORT_CFG_PERIPHERAL_PIN
                   | (uint32_t) IOPORT_PERIPHERAL_OSPI) },
